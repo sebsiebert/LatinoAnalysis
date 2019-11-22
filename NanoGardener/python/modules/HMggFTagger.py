938d733bef73d552072bc7d306af4d3103d90405
Sebastian Siebert <sebastian.siebert@rwth-aachen.de>
2019-11-22 17:33:18 +0100

    implementation progress, computeP not working

--- NanoGardener/python/modules/HMggFTagger.py
+++ NanoGardener/python/modules/HMggFTagger.py
@@ -1,7 +1,9 @@
 import ROOT
 import os
+import math
 
-from ZZMatrixElement.MELA.mela import Mela, SimpleParticle_t, SimpleParticleCollection_t, TVar
+from ZZMatrixElement.MELA.mela import Mela, SimpleParticleCollection_t, TVar
+SimpleParticle_t = ROOT.SimpleParticle_t
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
@@ -15,8 +17,10 @@ class HMggFTaggerClass(Module):
     Add extra variables to NANO tree
     '''
     def __init__(self):
-        self.LHCsqrts_ = 13.
-        self.mh_ = 125
+        self.LHCsqrts = 13.
+        self.mh = 125
+        self.mela_con = 0.01
+        verbosity = ROOT.TVar.SILENT
 
         cmssw_base = os.getenv('CMSSW_BASE')
         cmssw_arch = os.getenv('SCRAM_ARCH')
@@ -24,14 +28,14 @@ class HMggFTaggerClass(Module):
         # Stuff needed:
         # Directory ZZMatrixElement: https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git / https://twiki.cern.ch/twiki/bin/view/CMS/MELAProject
         # Directory MelaAnalytics: https://github.com/usarica/MelaAnalytics.git
-        # ROOT.gSystem.AddIncludePath("-I"+self.cmssw_base+"/interface/")
-        # ROOT.gSystem.AddIncludePath("-I"+self.cmssw_base+"/src/")
-        # ROOT.gSystem.Load("libZZMatrixElementMELA.so")
-        # ROOT.gSystem.Load("libMelaAnalyticsCandidateLOCaster.so")
-        # ROOT.gSystem.Load(self.cmssw_base+"/src/ZZMatrixElement/MELA/data/"+cmssw_arch+"/libmcfm_705.so")
+        ROOT.gSystem.AddIncludePath("-I"+cmssw_base+"/interface/")
+        ROOT.gSystem.AddIncludePath("-I"+cmssw_base+"/src/")
+        ROOT.gSystem.Load("libZZMatrixElementMELA.so")
+        ROOT.gSystem.Load("libMelaAnalyticsCandidateLOCaster.so")
+        ROOT.gSystem.Load(cmssw_base+"/src/ZZMatrixElement/MELA/data/"+cmssw_arch+"/libmcfm_705.so")
 
         # self.mela = ROOT.Mela(LHCsqrts_, mh_)
-        self.mela = Mela(self.LHCsqrts_, self.mh_)
+        self.mela = Mela(self.LHCsqrts, self.mh, verbosity)
 
     def beginJob(self):
         pass
@@ -41,7 +45,7 @@ class HMggFTaggerClass(Module):
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        self.out.branch("IsggF", "O")
+        self.out.branch("ggF_MELA_var", "F")
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -49,35 +53,151 @@ class HMggFTaggerClass(Module):
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
 
-        IsBoosted   = getattr(event, 'IsBoosted')
-        IsResolved  = getattr(event, 'IsBoosted')
-        IsTopTagged = getattr(event, 'IsTopTagged')
-        IsVbfFat    = getattr(event, 'IsVbfFat')
-        IsVbfjj     = getattr(event, 'IsVbfjj')
+        IsBoosted  = getattr(event, 'IsBoosted')
+        IsResolved = getattr(event, 'IsBoosted')
+        IsVbfFat   = getattr(event, 'IsVbfFat')
+        IsVbfjj    = getattr(event, 'IsVbfjj')
 
-        Lept_col           = Collection(event, 'Lepton')
-        CFatJet_col        = Collection(event, 'CleanFatJet')
-        CJet_col           = Collection(event, 'CleanJet')
-        CleanJetNotFat_col = Collection(event, 'CleanJetNotFat')
-        Jet_col            = Collection(event, 'Jet')
+        Lept_col    = Collection(event, 'Lepton')
+        CJet_col    = Collection(event, 'CleanJet')
+        CFatJet_col = Collection(event, 'CleanFatJet')
+        WJet_col    = Collection(event, 'CleanFatJetPassMBoosted')
+
+        Wlep_pt_Puppi   = getattr(event, "Wlep_pt_Puppi")
+        Wlep_eta_Puppi  = getattr(event, "Wlep_eta_Puppi")
+        Wlep_phi_Puppi  = getattr(event, "Wlep_phi_Puppi")
+        Wlep_mass_Puppi = getattr(event, "Wlep_mass_Puppi")
+
+        Wjj_ClJet0_idx  = getattr(event, "idx_j1")
+        Wjj_ClJet1_idx  = getattr(event, "idx_j2")
 
         IsggF = False
+
+
 
         daughter_coll   = SimpleParticleCollection_t()
         associated_coll = SimpleParticleCollection_t()
 
+        # daughter particles
+        #######################
+        ## leptonic part
+        #######################
+        chargedLeptonID = Lept_col[0]['pdgId']
+        chargedLeptonLV = ROOT.TLorentzVector()
+        chargedLeptonLV.SetPtEtaPhiM(
+                Lept_col[0]['pt'],
+                Lept_col[0]['eta'],
+                Lept_col[0]['phi'],
+                0.0005 if abs(chargedLeptonID)==11 else 0.1057
+        )
+        chargedLepton = SimpleParticle_t(chargedLeptonID, chargedLeptonLV)
+        neutralLeptonID = chargedLeptonID
+        neutralLeptonID += neutralLeptonID / abs(neutralLeptonID) # increase abs(ID)
+        neutralLeptonLV = ROOT.TLorentzVector()
+        neutralLeptonLV.SetPtEtaPhiM(
+                Wlep_pt_Puppi,
+                Wlep_eta_Puppi,
+                Wlep_phi_Puppi,
+                Wlep_mass_Puppi
+        )
+        neutralLeptonLV = neutralLeptonLV - chargedLeptonLV
+        neutralLepton = SimpleParticle_t(neutralLeptonID, neutralLeptonLV)
+
+        daughter_coll.push_back(chargedLepton)
+        daughter_coll.push_back(neutralLepton)
+
+        # fill remaining leptons in associated_coll
+        for i in range(2, len(Lept_col)):
+            lepID = Lept_col[i]['pdgId']
+            lepLV = ROOT.TLorentzVector()
+            lepLV.SetPtEtaPhiM(
+                    Lept_col[i]['pt'],
+                    Lept_col[i]['eta'],
+                    Lept_col[i]['phi'],
+                    0.0005 if abs(chargedLeptonID)==11 else 0.1057
+            )
+            associated_coll.push_back(SimpleParticle_t(chargedLeptonID, chargedLeptonLV))
+
+        ######################
+        ## hadronic part
+        ######################
         if IsBoosted and IsVbfFat:
-            pass
+            jetID = 0
+            jetLV = ROOT.TLorentzVector()
+            jetLV.SetPtEtaPhiM(
+                    WJet_col[0]['pt'],
+                    WJet_col[0]['eta'],
+                    WJet_col[0]['phi'],
+                    WJet_col[0]['mass']
+            )
+            daughter_coll.push_back(SimpleParticle_t(jetID, jetLV))
+
+            # fill all CleanJetNotFat into associated_coll
+            for i in range(2, len(CFatJet_col)):
+                if i == WJet_col[0]['CFatJetIdx']: continue # don't add W jet twice
+                jetLV = ROOT.TLorentzVector()
+                jetLV.SetPtEtaPhiM(
+                        CFatJet_col[i]['pt'],
+                        CFatJet_col[i]['eta'],
+                        CFatJet_col[i]['phi'],
+                        CFatJet_col[i]['mass']
+                )
+                associated_coll.push_back(SimpleParticle_t(0, jetLV))
+
 
         elif IsResolved and IsVbfjj:
-            pass
+            jetID = 0
+            jet0LV = ROOT.TLorentzVector()
+            jet0LV.SetPtEtaPhiM(
+                    CJet_col[Wjj_ClJet0_idx]['pt'],
+                    CJet_col[Wjj_ClJet0_idx]['eta'],
+                    CJet_col[Wjj_ClJet0_idx]['phi'],
+                    CJet_col[Wjj_ClJet0_idx]['mass']
+            )
+            jet1LV = ROOT.TLorentzVector()
+            jet1LV.SetPtEtaPhiM(
+                    CJet_col[Wjj_ClJet1_idx]['pt'],
+                    CJet_col[Wjj_ClJet1_idx]['eta'],
+                    CJet_col[Wjj_ClJet1_idx]['phi'],
+                    CJet_col[Wjj_ClJet1_idx]['mass']
+            )
+            daughter_coll.push_back(SimpleParticle_t(jetID, jet0LV))
+            daughter_coll.push_back(SimpleParticle_t(jetID, jet1LV))
 
-        self.out.fillBranch("IsggF", IsggF)
+            # fill all other CleanJet into associated_coll
+            for i in range(2, len(CJet_col)):
+                if i == WWjj_ClJet0_idx or i == WWjj_ClJet1_idx: continue # don't add twice
+                jetLV = ROOT.TLorentzVector()
+                jetLV.SetPtEtaPhiM(
+                        CJet_col[i]['pt'],
+                        CJet_col[i]['eta'],
+                        CJet_col[i]['phi'],
+                        CJet_col[i]['mass']
+                )
+                associated_coll.push_back(SimpleParticle_t(0, jetLV))
+
+
+
+        # compute discriminator
+        self.mela.setCandidateDecayMode(TVar.CandidateDecay_WW)
+        self.mela.setInputEvent(daughter_coll, associated_coll, 0, 0)
+        self.mela.setCurrentCandidateFromIndex(0)
+
+        self.mela.setProcess(TVar.bkgWW, TVar.MCFM, TVar.ZZQQB)
+        p_qqWW = self.mela.computeP(False)
+        self.mela.setProcess(TVar.HSMHiggs, TVar.MCFM, TVar.ZZGG)
+        p_ggH = self.mela.computeP(False)
+
+
+        melaDiscriminant = 1/(1+self.mela_con*(p_qqWW/p_ggH)) if p_ggH > 0 else 0
+        self.mela.resetInputEvent()
+
+
+        self.out.fillBranch("ggF_MELA_var", melaDiscriminant)
         return True
 
-# define modules using the syntax
+# to avoid having them loaded when not needed, define modules using the syntax:
 # 'name = lambda : constructor'
-# to avoid having them loaded when not needed
 HMggFTagger = lambda : HMggFTaggerClass()
 
 """
